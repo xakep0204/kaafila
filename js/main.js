b7f988307449ee622721bcb14ccea47dbbfc2446
xakep0205 <abhimanyu@attrish.in>
2019-07-11 23:09:34 +0530

    edit 2

--- js/main.js
+++ js/main.js
@@ -37,7 +37,7 @@ jQuery(document).ready(function( $ ) {
   // Initialize Venobox
   $('.venobox').venobox({
     bgcolor: '',
-    overlayColor: 'rgba(6, 12, 34, 0.85)',
+    overlayColor: 'rgba(6, 12, 34, 0.50)',
     closeBackground: '',
     closeColor: '#fff'
   });
