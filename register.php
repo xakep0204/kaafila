30a2853674e2099d020165e04670b93dcb619245
xakep0205 <abhimanyu@attrish.in>
2019-08-19 06:09:24 +0530

    init vars

--- register.php
+++ register.php
@@ -1,8 +1,14 @@
 <?php
 
 $school_name = $_POST['school_name'];
-echo $school_name."\n";
-$
+$school_email = $_POST['school_email'];
+$school_address = $_POST['school_address'];
+$school_phone = $_POST['school_phone'];
+$main_contact = $_POST['main_contact'];
+$main_contact_w = $_POST['main_contact_w'];
+$teacher_escort_1 = $_POST['teacher_escort_1'];
+$teacher_escort_1_w = $_POST['teacher_escort_1_w'];
+
 
 
 
