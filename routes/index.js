5bef8d35d76089b680a79ce23788b4329649df68
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-07-05 22:04:19 +0530

    navbar

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {title:"Express"});
+    res.render('index', {
+        title: "Express",
+        headerStyles: '/partials/navbar_css'
+    });
 });
 
 module.exports = router;
