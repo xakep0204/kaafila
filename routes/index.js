ef2492d3fc122c44e1f2acac8416206ae9d5c930
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-07-05 22:26:40 +0530

    navbar fix

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', {
         title: "Express",
-        headerStyles: '/partials/navbar_css'
+        headerStyles: ['/css/navbar.css']
     });
 });
 
