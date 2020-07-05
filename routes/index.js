4be1cf1cbfaee09c46ed5d0c290a05a9ed726f15
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-07-05 21:29:31 +0530

    ok

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index',{layouts: 'main',templates: 'main'});
+  res.render('index', {title:"Express"});
 });
 
 module.exports = router;
