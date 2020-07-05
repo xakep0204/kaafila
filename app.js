ab8966417444696a05a37a0c5f645d277dcf2900
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-07-05 20:21:20 +0530

    hbs update 1

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('layouts', path.join(__dirname, 'views/layouts'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
