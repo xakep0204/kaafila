3148bc83743e29ecf58b223a479f366489179172
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-07-06 14:44:53 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-<<<<<<< HEAD
-  res.render('index', {title:"Kaafila"});
-=======
     res.render('index', {
         title: "Express",
         headerStyles: ['/css/navbar.css', '/css/hero-banner.css'],
         scripts: ['/js/navbar.js']
     });
->>>>>>> c1ab19f793e4c08a3ed92029800c92b5a120b713
 });
 
 module.exports = router;
