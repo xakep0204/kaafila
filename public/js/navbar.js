144c2b298c6d43857b9791f2cdaeb07f32a5f119
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-07-18 10:38:40 +0530

    navbar and hero

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -3,17 +3,18 @@ var $header = $("header.hero");
 
 $(document).ready(function() {
     $(".ui.toggle.button").click(function() {
-      if ($(this).scrollTop() <= $header.height()) {
+      if (!($(document).scrollTop()+54 > $header.height())) {
         $nav.toggleClass('scrolled');
       }
       $(".mobile.only.grid .ui.vertical.menu").toggle(100);
     });
 
     $(".ui.dropdown").dropdown();
+    $nav.toggleClass('scrolled', $(this).scrollTop()+54 > $header.height());
   });
 
 $(function() {
   $(document).scroll(function() {
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $header.height());
+    $nav.toggleClass('scrolled', $(this).scrollTop()+54 > $header.height());
     });
 });
