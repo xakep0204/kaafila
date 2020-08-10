aa8d79f3b03db67a072f790b73c87521ead24114
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-08-10 16:22:41 +0530

    quick 2

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var fs = require('fs');
 
 
-var galleryDirectoryPath = path.join(__dirname, '../../kaafila-img/gallery');
+var galleryDirectoryPath = path.join(__dirname, '../public/img/kaafila-img/gallery');
 var sponsorsDirectoryPath = path.join(__dirname, '../public/img/sponsors');
 
 router.get('/', function(req, res, next) {
@@ -25,9 +25,10 @@ router.get('/about', function(req, res, next) {
             return console.log('Unable to scan directory: ' + err);
         } 
         files.forEach(function (file) {
-            if (file.slice(-3) == 'jpg' || file.slice(-3) == 'png' || file.slice(-3) == 'gif') {
+            if (file.slice(-3) == 'jpg' || file.slice(-3) == 'png' || file.slice(-3) == 'JPG' || file.slice(-3) == 'gif') {
                 imageObject = {'path': file, 'caption': file.slice(0, -4)}
                 galleryImages.push(imageObject);
+                console.log(imageObject)
             }
         });
     });
@@ -63,6 +64,9 @@ router.get('/about', function(req, res, next) {
 router.get('/bread-and-circuses', function(req, res, next) {
     res.render('event', {
         title: "Bread and Circuses - Kaafila",
+        header_bnc: true,
+        header: 'Bread <span class="princess-sofia">&</span> Circuses',
+        headerFont: 'rye',
         bannerName: "bread-and-circuses",
         active_bnc: true,
         styles: [
@@ -78,6 +82,8 @@ router.get('/bread-and-circuses', function(req, res, next) {
 router.get('/folk-fluence', function(req, res, next) {
     res.render('event', {
         title: "Folk Fluence - Kaafila",
+        header: 'Folk Fluence',
+        headerFont: 'ardagh',
         bannerName: "folk-fluence",
         active_ff: true,
         styles: [
@@ -93,6 +99,8 @@ router.get('/folk-fluence', function(req, res, next) {
 router.get('/iridescence', function(req, res, next) {
     res.render('event', {
         title: "Iridescence - Kaafila",
+        header: 'Iridescence',
+        headerFont: 'apple-garamond',
         bannerName: "iridescence",
         active_i: true,
         styles: [
@@ -108,6 +116,8 @@ router.get('/iridescence', function(req, res, next) {
 router.get('/strings-attached', function(req, res, next) {
     res.render('event', {
         title: "Strings Attached - Kaafila",
+        header: 'Strings Attached',
+        headerFont: 'welcome',
         bannerName: "strings-attached",
         active_sa: true,
         styles: [
