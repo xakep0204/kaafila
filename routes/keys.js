55f9556a4d1ddcfc4d83207f07f91f27104b066b
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-09-05 19:47:54 +0530

    key

--- routes/keys.js
+++ routes/keys.js
@@ -1,16 +1,3 @@
 module.exports = {
-  firebase: {
-    apiKey: "AIzaSyBVLs6Abc0Nt0LyZQHlQd2NcL5y5FiLtWc",
-    authDomain: "kaafila-8cc49.firebaseapp.com",
-    databaseURL: "https://kaafila-8cc49.firebaseio.com",
-    projectId: "kaafila-8cc49",
-    storageBucket: "kaafila-8cc49.appspot.com",
-    messagingSenderId: "821115427097",
-    appId: "1:821115427097:web:492776918ba67392ea4d88",
-    measurementId: "G-YT3TV95M5Y"
-  },
-  session: {
-      cookieKey: 'loremipsum'
-  },
   serviceAccount: '/users/aditya/dev/kaafila-security/serviceAccount.json'
 };
