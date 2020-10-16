f9daff68a80f3642317a045528dc86dfb0fad2c4
Aditya Kulshrestha <adi.kul358@gmail.com>
2020-10-16 14:13:57 +0530

    exhibition fuckup

--- public/js/event.js
+++ public/js/event.js
@@ -1,1 +1,7 @@
 $(".ui.embed").embed();
+
+$(".conceptnote").click(function () {
+  $(`#conceptnote-modal .ui.embed`).attr("data-url", $(this).attr('data-pdfLink'))
+  $(`#conceptnote-modal .ui.embed`).embed()
+  $(`#conceptnote-modal`).modal("show");
+})
